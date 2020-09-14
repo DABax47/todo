18d0150a866627a2b0867137f97f38543738d4bb
DABax47 <45886982+DABax47@users.noreply.github.com>
2020-09-13 20:28:49 -0400

    Updates

---
+++ precache-manifest.b896570113cdb1ccc1cac63e408158fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7c16e8b434e640d9bd77ae175dac98d",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "336a9c166ecc6801efcf",
+    "url": "/todo/static/css/main.4ed58387.chunk.css"
+  },
+  {
+    "revision": "262c788b11e23e4bffdf",
+    "url": "/todo/static/js/2.84396668.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.84396668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "336a9c166ecc6801efcf",
+    "url": "/todo/static/js/main.185365ea.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
